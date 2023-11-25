0c567212c562c2b5464d1f5563abc03fe3d89bc9
manavumd <manavg@umd.edu>
2023-11-25 15:01:36 -0500

    changed ip address for aws

--- src/main/java/com/example/coursecontentmanagementservice/Model/Constants.java
+++ src/main/java/com/example/coursecontentmanagementservice/Model/Constants.java
@@ -1,6 +1,5 @@
 package com.example.coursecontentmanagementservice.Model;
 
 public class Constants {
-
-    public static final String AWS_IP = "35.171.189.199";
+    public static final String AWS_IP = "3.92.127.22";
 }
