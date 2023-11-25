c53f3ee8598a8deda4046bb3534213d19f6fe946
manavumd <manavg@umd.edu>
2023-11-25 17:37:05 -0500

    added AWS IP to localhost

--- src/main/java/com/example/coursecontentmanagementservice/Model/Constants.java
+++ src/main/java/com/example/coursecontentmanagementservice/Model/Constants.java
@@ -1,5 +1,5 @@
 package com.example.coursecontentmanagementservice.Model;
 
 public class Constants {
-    public static final String AWS_IP = "3.92.127.22";
+    public static final String AWS_IP = "localhost";
 }
